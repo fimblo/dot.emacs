aa46364cb0979c7dabe9a4df4a533bd5ebb25e7e
Mattias Jansson <fimblo@yanson.org>
2018-10-10 13:17:27 +0200

    Added package-initialise. also added comments

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
-(require 'org)
-;(org-babel-load-file "/home/fimblo/new-emacs/.emacs.d/setup.org")
-(org-babel-load-file (expand-file-name "~/.emacs.d/setup.org"))
+;; Minimal config here. The rest will be in setup.org
+(package-initialize)			; This needs to be early in config
+(require 'org)				; Load org so we can use babel
+(org-babel-load-file			; Load my setup file
+ (expand-file-name "~/.emacs.d/setup.org")
+ )
+;; EOF
