729e99b33c4b25fbcea3daf5efe490475e03be70
Mattias Jansson <fimblo@yanson.org>
2022-08-26 19:26:19 +0200

    cleaned with help of flycheck

--- bin/start_emacs.sh
+++ bin/start_emacs.sh
@@ -21,22 +21,22 @@
 deref () {
     arg=$1
     if [ -h "$arg" ] ; then
-	l=$(readlink $arg)
-	if $(echo $l | grep -q ^/) ; then
-	    echo $(deref $l)
+	l=$(readlink "$arg")
+	if echo "$l" | grep -q ^/ ; then
+      deref "$l"
 	else
-	    p=$(dirname $arg)
-	    echo $(deref $p/$l)
+	    p=$(dirname "$arg")
+        deref "$p/$l"
 	fi
     else
-	echo $arg
+	echo "$arg"
     fi
 }
 
 if ! which emacs > /dev/null 2>&1 ; then
     echo "I dont think you've installed emacs. Starting vim instead"
-    if [ $DISPLAY ] ; then
-        xterm -e vim $@ &
+    if [ "$DISPLAY" ] ; then
+        xterm -e vim "$@" &
         echo "Emacs was not installed. I ran vim instead."
     else
       echo "Emacs is not installed."
@@ -44,32 +44,31 @@ if ! which emacs > /dev/null 2>&1 ; then
     exit
 fi
 
-emacsbin=$(deref $(which emacs))
-emacsclientbin=$(deref $(which emacsclient))
+emacsbin=$(deref "$(which emacs)")
+emacsclientbin=$(deref "$(which emacsclient)")
 
 myVersion="GNU Emacs 28"
 if ! $emacsbin --version | grep -q "$myVersion" ; then
     echo "This is not version '$myVersion'. Starting whatever version and not starting server."
-    $emacsbin $@ &
+    $emacsbin "$@" &
     exit 0 
 fi
 
 
 # Start emacs daemon if it hasnt already been started.
-! ps -ef |grep -v gpg-agent | grep [eE]macs.*--daemon > /dev/null 2>&1 && \
+! ps -ef |grep -v "gpg-agent" | grep "[eE]macs.*--daemon" > /dev/null 2>&1 && \
   $emacsbin --daemon
 
 
 # If this script was called by name containing string "wait" then we
 # wait till user hits 'c-x 5 0' before exiting.
 nowait=" -n "
-[[ "$(basename $0)" =~ "wait" ]] && unset nowait
+[[ "$(basename "$0")" =~ "wait" ]] && unset nowait
 
 # If this script was called by name containing string 'tty' then start
 # emacs in the current tty. Otherwise, start in new X window.
 displaymode=" -c "
-[[ "$(basename $0)" =~ "tty" ]] && displaymode=' -t ' && unset nowait
+[[ "$(basename "$0")" =~ "tty" ]] && displaymode=' -t ' && unset nowait
 
-$emacsclientbin $nowait $displaymode $@
-
+$emacsclientbin "$nowait" "$displaymode" "$@"
 
