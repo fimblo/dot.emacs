a285316dd37a6bad845be78d9c717a7d6e8bd549
Mattias Jansson <fimblo@yanson.org>
2024-02-01 22:43:34 +0100

    refactor: clean emacs start script
    ... and add some documentation

--- bin/start_emacs.sh
+++ bin/start_emacs.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
+# Simplified Emacs starter script.
+#
+# Use: Create symlinks to this script in your PATH to launch
+# emacsclient in different modes:
+#
+# 1. Terminal mode:  ln -s /path/to/script emt
+# 2. New window mode: ln -s /path/to/script emc
+# 3. Existing window mode: ln -s /path/to/script em
+#
+# emt and emc initiates an emacs server if not already running and
+# opens files accordingly. em assumes a server is running.
 
-source "$DENV/functions.lib.sh"
-ME=$(basename $0)
+ME=$(basename "$0")
 
 if ! which emacsclient > /dev/null 2>&1 ; then
-    echo "Can't find 'emacsclient' in PATH. Starting vim instead."
-    vim "$@"
-    exit
+  echo "Can't find 'emacsclient' in PATH. Starting vim instead."
+  vim "$@"
+  exit
 fi
 
-client=$(deref "$(which emacsclient)")      # location of emacsclient
+client=$(realpath "$(which emacsclient)")
 
-# run in terminal if called as emt
 if [[ $ME == "emt" ]] ; then
+  # run in terminal if called as emt
   $client -a '' -t "$@"
   exit 0
-fi
 
-# create new frame if run as emc
-if [[ $ME == "emc" ]] ; then
+elif [[ $ME == "emc" ]] ; then 
+  # create new frame if run as emc
   client="${client} -c"
+
+else 
+  # any other name opens emacs in an existing window
+  true
 fi
 
 
