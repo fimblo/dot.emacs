12334ea41fc3b1d7c95ae95b487f2850f83623b9
Mattias Jansson <fimblo@yanson.org>
2024-02-01 21:24:55 +0100

    style: tab to space

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Minimal config here. The rest will be in setup.org
-(package-initialize)	 ; This needs to be early in config
-(require 'org)			 ; Load org so we can use babel
+(package-initialize)     ; This needs to be early in config
+(require 'org)           ; Load org so we can use babel
 
 (org-babel-load-file     ; Load my setup file
  (expand-file-name (concat (file-name-directory user-init-file) "setup.org")))
