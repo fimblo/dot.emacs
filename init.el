f3221ff4aa43a26a5e9ac9f59babeaf1471bda13
Mattias Jansson <fimblo@yanson.org>
2024-02-14 17:29:29 +0100

    fix: load setup.org properly

--- init.el
+++ init.el
@@ -2,6 +2,23 @@
 (package-initialize)     ; This needs to be early in config
 (require 'org)           ; Load org so we can use babel
 
-(org-babel-load-file     ; Load my setup file
- (expand-file-name (concat default-directory "setup.org")))
+;; Then we're running emacs in interactive mode (the normal way) and
+;; we'll load the file 'setup.org' which resides next to the
+;; init-file.
+;;
+;; If we can't find user-init-file, then we're running in batch-mode.
+;; Then we use the variable `load-true-file-name` since that should
+;; exist.
+
+(defconst sys/emacs-root
+  (expand-file-name
+    (file-name-directory
+     (if user-init-file user-init-file load-true-file-name))
+    )
+  "Points at the directory where the emacs configuration files reside."
+  )
+
+(org-babel-load-file
+ (concat sys/emacs-root "setup.org"))
+
 ;; EOF
